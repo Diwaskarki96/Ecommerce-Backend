f43d8d3a0e57a549c2362fc16a0110d52d8a9f06
Diwash Karki <diwaskarki96@gmail.com>
2024-03-26 13:59:01 +0545

    added delete all cart

--- modules/cart/cart.api.js
+++ modules/cart/cart.api.js
@@ -10,16 +10,20 @@ const router = require("express").Router();
 router.post("/add", isBuyer, async (req, res, next) => {
   try {
     const cartData = req.body;
-    console.log(cartData);
     const validateData = await addItemToCartValidation.validate(cartData);
     const isValidMongoId = mongoose.isValidObjectId(cartData.productId);
     if (!isValidMongoId) throw new Error("Invalid Product ID");
     const product = await productController.findId({ id: cartData.productId });
     if (!product) throw new Error("Product not found!");
-    console.log(product);
     if (cartData.orderQuantity > product.availableQuantity)
       throw new Error("Order  quantity exceeds available quantity.");
-    console.log(req.loggedInUserId);
+
+    //buyer cannot add cart twice in product after doing one time. buyer should go to add to cart to increase card
+    const cartItem = await cartModel.findOne({
+      buyerId: req.loggedInUserId,
+      productId: cartData.productId,
+    });
+    if (cartItem) throw new Error("This item already in your cart.");
     // const createCart = await cartController.add(validateData);
     const createCart = await cartModel.create({
       buyerId: req.loggedInUserId,
@@ -27,6 +31,16 @@ router.post("/add", isBuyer, async (req, res, next) => {
       orderQuantity: cartData.orderQuantity,
     });
     res.json({ msg: "success", data: createCart });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete("/clear", isBuyer, async (req, res, next) => {
+  try {
+    const loggedInUserId = req.loggedInUserId;
+    const deleteCart = await cartModel.deleteMany({ buyerId: loggedInUserId });
+    res.json({ msg: "clear is working", data: deleteCart });
   } catch (e) {
     next(e);
   }
