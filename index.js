923f5367233286c4201af2219bcdda18dc1f31b9
Diwash Karki <diwaskarki96@gmail.com>
2024-03-27 13:30:43 +0545

    minor change

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT;
 const DB_URL = process.env.DB_URL;
 mongoose.connect(DB_URL).then(() => {
-  console.log("Database is conneted...");
+  console.log("Database is connected...");
 });
 app.use(express.json());
 app.use("/user", userRouter);
