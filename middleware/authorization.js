af9758f6b1915bbad425c75e879bdf6211953efc
Diwash Karki <diwaskarki96@gmail.com>
2024-03-31 13:11:07 +0545

    minor env change

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -7,7 +7,7 @@ const isSeller = async (req, res, next) => {
     const splittedValue = authorization?.split(" ");
     const token = splittedValue?.length === 2 ? splittedValue[1] : undefined;
     if (!token) throw new Error("Unauthorized");
-    const payload = jwt.verify(token, "shhhhh");
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
     const user = await userController.findByEmail({ email: payload.email });
     if (!user) throw new Error("User not found");
     if (user.role !== "seller") throw new Error("Unauthorized");
@@ -24,7 +24,7 @@ const isBuyer = async (req, res, next) => {
     const splittedValue = authorization?.split(" ");
     const token = splittedValue?.length === 2 ? splittedValue[1] : undefined;
     if (!token) throw new Error("Unauthorized");
-    const payload = jwt.verify(token, "shhhhh");
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
     const user = await userController.findByEmail({ email: payload.email });
     if (!user) throw new Error("User not found");
     if (user.role !== "buyer") throw new Error("Unauthorized");
@@ -41,7 +41,7 @@ const isUser = async (req, res, next) => {
     const splittedValue = authorization?.split(" ");
     const token = splittedValue?.length === 2 ? splittedValue[1] : undefined;
     if (!token) throw new Error("Unauthorized");
-    const payload = jwt.verify(token, "shhhhh");
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
     const user = await userController.findByEmail({ email: payload.email });
     if (!user) throw new Error("User not found");
     req.loggedInId = req._id;
